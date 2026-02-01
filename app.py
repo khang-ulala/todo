377d72dc64669198bad70bf534a56380e3270eb3
khang_admin <kundiep1105@gmail.com>
2026-02-01 17:56:28 +0700

    this commit

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from markupsafe import escape
+import pymongo
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+uri = "mongodb+srv://kundiep1105_db_user:hello123@study.v5oaz4f.mongodb.net/?appName=Study"
+try:
+    print('connect done')
+    client = pymongo.MongoClient(uri)
+except Exception:
+    print('error' + Exception)
+
+# truy cap vao co so du lieu khang_todo
+database = client.get_database('khang_todo')
+# truy cap vao collection todoList thong qua database khang_todo
+todo = database.get_collection('todoList')
+# truy cap vao collection users thong qua database khang_todo
+users = database.get_collection('users')
+
+todo_list = []
+users_value = {}
+
+# duyet qua collection va them vao list
+for x in todo.find():
+    todo_list.append(x)
+
+# duyet qua collection va them vao dictionary
+for y in users.find():
+    name = y['name']
+    email = y['email']
+    password = y['password']
+    users_value.update({name:password})
+
+print(todo_list)
+print(users_value)
 
 @app.route('/')
 def home():
@@ -17,6 +50,43 @@ def signup():
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+'''trang web dung de nhap ten va mat khau nguoi dung'''
+@app.route('/handle_get', methods=['GET','POST'])
+def handle_get():
+    # lay du lieu tu tren form va kiem tra
+    # xem username va password co o trong mongodb hay khong
+    error = None
+    print('handle_get')
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        print("username: " + username + " password: " + password)
+        print("users_value: ", users_value)
+        if username in users_value and users_value[username] == password:
+            # successful -> main.html
+            return render_template("main.html")
+        else:
+            # username not found
+            return render_template("login.html", error = error)
+    else:
+        # not a POST request
+        return render_template("login.html")
+
+@app.route('/handle_post', methods=['GET','POST'])
+def handle_post():
+    error = None
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form["email"]
+        if username != "" and password != "" and email != "":
+            add_user = {"name": username, "password":password, "email":email}
+            x = users.insert_one(add_user)
+            return render_template("main.html")
+        else:
+            return render_template("signup.html")
+    return render_template("signup.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
